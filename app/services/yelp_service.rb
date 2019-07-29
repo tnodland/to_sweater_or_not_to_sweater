fd2064c33cb29ae68f9cc71ad6d84e51310084dc
Trevor Nodland <trevor.nodland@gmail.com>
2019-07-29 09:52:55 -0600

    adds unrefactored method to returning resturaunts at a location that will be open at a specific time

--- app/services/yelp_service.rb
+++ app/services/yelp_service.rb
@@ -1,5 +1,13 @@
 class YelpService
+  def initialize
+    @yelp_key = ENV['YELP-API-KEY']
+  end
+
   def resturaunts_open_at(location, time, limit)
-    conn = Faraday.new()
+    conn = Faraday.new("https://api.yelp.com/v3/businesses/search?term=restaurants&location=#{location}&limit=#{limit}&open_at=#{time}") do |f|
+      f.adapter Faraday.default_adapter
+      f.headers['Authorization'] = "Bearer #{@yelp_key}"
+    end
+    conn.get
   end
 end
