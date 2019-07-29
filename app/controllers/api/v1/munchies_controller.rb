ac9be572a79cb43ec070fcc686b31376388a5763
Trevor Nodland <trevor.nodland@gmail.com>
2019-07-29 11:11:33 -0600

    gets code working for specified user story, resets cassettes, moving to refactor

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::MunchiesController < ActionController::API
   def index
     facade = MunchieFacade.new(params[:start], params[:end], params[:food])
-    binding.pry
-    render json: MunchieSerializer.new
+    render json: MunchieSerializer.new(facade.restaurants)
   end
 end
--- spec/requests/api/v1/munchie_spec.rb
+++ spec/requests/api/v1/munchie_spec.rb
@@ -1,10 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe "munchie request", :vcr do
+RSpec.describe "munchie request" do
   it "can send 3 resturaunts that will be open at a desired location" do
     get '/api/v1/munchies?start=denver,co&end=pueblo,co&food=chinese'
 
     expect(response).to be_successful
-    binding.pry
+    parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed_response[:data]).to be_a(Array)
+    expect(parsed_response[:data][0]).to be_a(Hash)
+    expect(parsed_response[:data][0]).to have_key(:id)
   end
 end
