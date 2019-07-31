26f20fb65ba984831f4e1bb3b639d79ce2240902
Trevor Nodland <trevor.nodland@gmail.com>
2019-07-31 13:22:04 -0600

    fixes munchie spec

--- spec/requests/api/v1/munchie_spec.rb
+++ spec/requests/api/v1/munchie_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "munchie request" do
-  xit "can send 3 resturaunts that will be open at a desired location" do
+  it "can send 3 resturaunts that will be open at a desired location" do
     get '/api/v1/munchies?start=denver,co&end=pueblo,co&food=chinese'
 
     expect(response).to be_successful
     parsed_response = JSON.parse(response.body, symbolize_names: true)
 
-    expect(parsed_response[:data]).to be_a(Array)
-    expect(parsed_response[:data][0]).to be_a(Hash)
-    expect(parsed_response[:data][0]).to have_key(:id)
+    expect(parsed_response[:munchies][:data]).to be_a(Array)
+    expect(parsed_response[:munchies][:data][0]).to be_a(Hash)
+    expect(parsed_response[:munchies][:data][0]).to have_key(:id)
   end
 end
