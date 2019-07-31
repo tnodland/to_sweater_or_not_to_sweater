8446b84fae634d2bf6d74ef20872a8523b759ccf
Trevor Nodland <trevor.nodland@gmail.com>
2019-07-31 16:16:37 -0600

    adds detail to forecast spec assertions

--- spec/requests/api/v1/forecast_spec.rb
+++ spec/requests/api/v1/forecast_spec.rb
@@ -6,5 +6,17 @@ RSpec.describe 'getting forecasts', :vcr do
     get '/api/v1/forecast?location=denver,co'
 
     expect(response).to be_successful
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(json).to be_a(Hash)
+    expect(json).to have_key(:data)
+    expect(json[:data]).to have_key(:id)
+    expect(json[:data][:attributes]).to have_key(:current_weather)
+    expect(json[:data][:attributes]).to have_key(:daily_weather)
+    expect(json[:data][:attributes]).to have_key(:hourly_weather)
+    expect(json[:data][:attributes][:daily_weather][:data]).to be_a(Array)
+    expect(json[:data][:attributes][:daily_weather][:data].count).to eq(8)
+    expect(json[:data][:attributes][:hourly_weather][:data]).to be_a(Array)
+    expect(json[:data][:attributes][:hourly_weather][:data].count).to eq(49)
   end
 end
